b32c21213d1ae8996d131b2e9e0ee7ebae2c26cc
Ashleigh Wong <ashleighewong@gmail.com>
2024-09-16 12:10:19 -0400

    adding task_1

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,9 +1,9 @@
 import java.util.List;
 
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
-        long s = 0;
+        long s = 0; // changed int to long
+        // type being returned was not specfied when it is int, need to change to long
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
